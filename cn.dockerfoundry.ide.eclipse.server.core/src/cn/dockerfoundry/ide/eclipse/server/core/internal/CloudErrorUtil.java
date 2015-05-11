2b20472e0d9fc66f038d701b6ad27d0fd950450b
wangxn <osswangxining@163.com>
2015-05-11 17:12:37 +0800

    support Docker Container as Server

---
+++ cn.dockerfoundry.ide.eclipse.server.core/src/cn/dockerfoundry/ide/eclipse/server/core/internal/CloudErrorUtil.java
@@ -0,0 +1,249 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2015 Pivotal Software, Inc. 
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, 
+ * Version 2.0 (the "License"); you may not use this file except in compliance 
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ *  Contributors:
+ *     Pivotal Software, Inc. - initial API and implementation
+ *     Keith Chong, IBM - Support more general branded server type IDs.   Moved isCloudFoundryServer to CloudServerUtil.
+ ********************************************************************************/
+package cn.dockerfoundry.ide.eclipse.server.core.internal;
+
+import java.io.IOException;
+
+import javax.net.ssl.SSLPeerUnverifiedException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Utility to detect various types of HTTP errors, like 400 Bad Request errors.
+ * 
+ */
+public class CloudErrorUtil {
+
+	private CloudErrorUtil() {
+		// Util class
+	}
+
+	/**
+	 * Parses a error message if and only if the error is due to a request
+	 * failure because of a connection error. For example, if a request failed
+	 * due to 401, 403, or unknown host errors. Returns null otherwise.
+	 * @param e error to check if it is a connection error.
+	 * @return User-friendly error message IFF the error is due to a connection
+	 * error that resulted in a failed request to the server. Return null
+	 * otherwise.
+	 */
+	public static String getConnectionError(CoreException e) {
+
+//		String error = getInvalidCredentialsError(e);
+//		if (error == null) {
+//			if (isUnknownHostException(e)) {
+//				error = Messages.ERROR_UNABLE_TO_ESTABLISH_CONNECTION_UNKNOWN_HOST;
+//			}
+//			else if (isRestClientException(e)) {
+//				error = NLS.bind(Messages.ERROR_FAILED_REST_CLIENT, e.getMessage());
+//			}
+//		}
+
+		return null;
+	}
+
+	public static CoreException checkRestException(Throwable t) {
+//		String error = getInvalidCredentialsError(t);
+//		if (error == null) {
+//
+//			if (t instanceof ResourceAccessException && t.getCause() instanceof UnknownHostException) {
+//				error = Messages.ERROR_UNABLE_TO_ESTABLISH_CONNECTION_UNKNOWN_HOST;
+//			}
+//			else if (t instanceof RestClientException) {
+//				error = NLS.bind(Messages.ERROR_FAILED_REST_CLIENT, t.getMessage());
+//			}
+//
+//		}
+		return toCoreException(t);
+	}
+
+	/**
+	 * If the error is a server communication error, it wraps it in a
+	 * {@link CoreException} with user-friendly message. If the server
+	 * communciation error is due to {@link SSLPeerUnverifiedException}, the
+	 * latter is set as the cause for the wrapped CoreException. Otherwise,
+	 * returns the error as the cause in a CoreException. Always returns a
+	 * {@link CoreException}.
+	 * @param e
+	 * @return CoreException wrapper if communication error with server, or
+	 * otherwise return CoreException with original error as cause. Never null.
+	 * 
+	 */
+	public static CoreException checkServerCommunicationError(RuntimeException e) {
+		if (e != null && e.getCause() instanceof IOException) {
+			// Set the cause for SSL
+			if ((e.getCause() instanceof SSLPeerUnverifiedException)) {
+				return toCoreException(e.getCause());
+			}
+			else {
+				// Log other IO errors.
+				String errorMessage = NLS.bind(Messages.ERROR_UNABLE_TO_COMMUNICATE_SERVER, e.getMessage());
+				CloudFoundryPlugin.logError(errorMessage, e);
+				return new CoreException(new Status(IStatus.ERROR, CloudFoundryPlugin.PLUGIN_ID, errorMessage));
+			}
+		}
+		else {
+			return checkRestException(e);
+		}
+	}
+
+	/**
+	 * True if a request error occurred due to connection issues. For example,
+	 * 401, 403, or unknown host.
+	 * @param e error to check if it is a connection error.
+	 * @return true if a request error occurred due to connection issues. False
+	 * otherwise
+	 */
+	public static boolean isConnectionError(CoreException e) {
+		return getConnectionError(e) != null;
+	}
+
+	public static boolean isAppStoppedStateError(Exception e) {
+//		HttpClientErrorException badRequestException = getBadRequestException(e);
+//		if (badRequestException != null) {
+//			String message = getHttpErrorMessage(badRequestException);
+//
+//			if (message != null) {
+//				message = message.toLowerCase();
+//				return message.contains("state") && message.contains("stop"); //$NON-NLS-1$ //$NON-NLS-2$
+//			}
+//		}
+		return false;
+	}
+
+	public static String getHostTakenError(Exception e) {
+//		HttpClientErrorException badRequestException = getBadRequestException(e);
+//		if (badRequestException != null) {
+//			String message = getHttpErrorMessage(badRequestException);
+//
+//			if (message != null) {
+//				message = message.toLowerCase();
+//				if (message.contains("host") && message.contains("taken")) { //$NON-NLS-1$ //$NON-NLS-2$
+//					return Messages.ERROR_HOST_TAKEN;
+//				}
+//			}
+//		}
+		return null;
+	}
+
+	public static CoreException toCoreException(Throwable e) {
+//		if (e instanceof CloudFoundryException) {
+//			if (((CloudFoundryException) e).getDescription() != null) {
+//				return new CoreException(new Status(IStatus.ERROR, CloudFoundryPlugin.PLUGIN_ID, NLS.bind("{0} ({1})", //$NON-NLS-1$
+//						((CloudFoundryException) e).getDescription(), e.getMessage()), e));
+//			}
+//		}
+		return new CoreException(new Status(IStatus.ERROR, CloudFoundryPlugin.PLUGIN_ID, NLS.bind(
+				Messages.ERROR_PERFORMING_CLOUD_FOUNDRY_OPERATION, e.getMessage()), e));
+	}
+
+	/**
+	 * check 404 error.
+	 * @param t
+	 * @return true if 404 error. False otherwise
+	 */
+	public static boolean isNotFoundException(Throwable t) {
+		return false;
+	}
+
+	public static boolean isUnknownHostException(CoreException e) {
+		Throwable cause = e.getStatus().getException();
+	
+		return false;
+	}
+
+	public static CoreException toCoreException(String message) {
+		return toCoreException(message, null);
+	}
+
+	public static CoreException toCoreException(String message, Throwable error) {
+		if (message == null) {
+			message = Messages.ERROR_UNKNOWN;
+		}
+		if (error != null) {
+			if (error.getMessage() != null) {
+				message += " - " + error.getMessage(); //$NON-NLS-1$
+			}
+			return new CoreException(CloudFoundryPlugin.getErrorStatus(message, error));
+		}
+		else {
+			return new CoreException(CloudFoundryPlugin.getErrorStatus(message));
+		}
+	}
+
+	/**
+	 * Wraps the given error as a {@link CoreException} under these two
+	 * conditions:
+	 * 
+	 * <p/>
+	 * 1. Additional error message is provided that should be added to the
+	 * CoreException. If the error is already a CoreException, the cause of the
+	 * error will be added to the new CoreException
+	 * <p/>
+	 * 2. If no error message is provided, and the error is not a CoreException,
+	 * a new CoreExpception will be created with the error as the cause.
+	 * <p/>
+	 * Otherwise, if the error already is a CoreException and no additional
+	 * message is provided, the same CoreException is returned.
+	 * @param message optional. Additional message to add to the CoreException.
+	 * @param error must not be null
+	 * @param replaceMessage true if existing messages in the error should be
+	 * replaced by the given message. False if given message should be appended
+	 * to existing message
+	 * @return error as {@link CoreException}, with the additional error message
+	 * added if provided.
+	 */
+	public static CoreException asCoreException(String message, Throwable error, boolean replaceMessage) {
+
+		if (message != null) {
+
+			IStatus oldStatus = error instanceof CoreException ? ((CoreException) error).getStatus() : null;
+
+			IStatus newStatus = null;
+			if (oldStatus == null) {
+				newStatus = CloudFoundryPlugin.getErrorStatus(message, error);
+			}
+			else {
+				String enhancedMessage = replaceMessage ? message : message + " - " + oldStatus.getMessage(); //$NON-NLS-1$
+				newStatus = new Status(oldStatus.getSeverity(), oldStatus.getPlugin(), oldStatus.getCode(),
+						enhancedMessage, oldStatus.getException());
+			}
+
+			return new CoreException(newStatus);
+		}
+		else {
+			if (error instanceof CoreException) {
+				return (CoreException) error;
+			}
+			else {
+				return new CoreException(CloudFoundryPlugin.getErrorStatus(error));
+			}
+		}
+	}
+
+	public static String getCloudFoundryErrorMessage(Exception cfe) {
+		return null;
+	}
+
+}
---
+++ cn.dockerfoundry.ide.eclipse.server.core/src/cn/dockerfoundry/ide/eclipse/server/core/internal/client/PushApplicationOperation.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Pivotal Software, Inc. 
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, 
+ * Version 2.0 (the "License"); you may not use this file except in compliance 
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ *  Contributors:
+ *     Pivotal Software, Inc. - initial API and implementation
+ ********************************************************************************/
+package cn.dockerfoundry.ide.eclipse.server.core.internal.client;
+
+import java.util.Arrays;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.wst.server.core.IModule;
+import org.eclipse.wst.server.core.IServer;
+
+import cn.dockerfoundry.ide.eclipse.server.core.internal.CloudFoundryPlugin;
+import cn.dockerfoundry.ide.eclipse.server.core.internal.CloudFoundryServer;
+import cn.dockerfoundry.ide.eclipse.server.core.internal.Messages;
+
+/**
+ * Operation publish an application. If the application is already deployed and
+ * synchronised, it will only update the mapping between the module and the
+ * {@link CloudApplication}.
+ * 
+ * <p/>
+ * 1. Prompts for deployment information.
+ * <p/>
+ * 2. Creates the application if the application does not currently exist in the
+ * server
+ * <p/>
+ * 3. Starts the application if specified in the deployment configuration for
+ * the application.
+ * <p/>
+ * If the application is already published (it exists in the server), it will
+ * ONLY update the published cloud application mapping in the
+ * {@link CloudFoundryApplicationModule}. It will NOT re-create, re-publish, or
+ * restart the application.
+ * <p/>
+ *
+ */
+public class PushApplicationOperation extends StartOperation {
+
+	/**
+	 * 
+	 */
+
+	public PushApplicationOperation(CloudFoundryServerBehaviour behaviour, IModule[] modules) {
+		super(behaviour, false, modules);
+	}
+
+	@Override
+	protected DeploymentConfiguration prepareForDeployment(CloudFoundryApplicationModule appModule,
+			IProgressMonitor monitor) throws CoreException {
+		// If the app is already published, just refresh the application
+		// mapping.
+		int moduleState = getBehaviour().getServer()
+				.getModulePublishState(new IModule[] { appModule.getLocalModule() });
+		if (appModule.isDeployed() && moduleState == IServer.PUBLISH_STATE_NONE) {
+
+			getBehaviour().printlnToConsole(appModule, Messages.CONSOLE_APP_FOUND);
+
+			getBehaviour().printlnToConsole(appModule,
+					NLS.bind(Messages.CONSOLE_APP_MAPPING_STARTED, appModule.getDeployedApplicationName()));
+			try {
+				getBehaviour().updateCloudModule(appModule.getDeployedApplicationName(), monitor);
+				getBehaviour().printlnToConsole(appModule,
+						NLS.bind(Messages.CONSOLE_APP_MAPPING_COMPLETED, appModule.getDeployedApplicationName()));
+
+			}
+			catch (CoreException e) {
+				// Do not log the error. The application may not exist
+				// anymore. If it is a network error, it will become evident
+				// in further steps
+			}
+		}
+		else {
+			try {
+				CloudFoundryServer cloudServer = getBehaviour().getCloudFoundryServer();
+
+				// prompt user for missing details
+				return CloudFoundryPlugin.getCallback().prepareForDeployment(cloudServer, appModule, monitor);
+			}
+			catch (OperationCanceledException oce) {
+				// Prepare for deployment prompts the user for missing
+				// information for a non-published app. If a user
+				// cancels
+				// delete the application module
+				getBehaviour().getCloudFoundryServer().doDeleteModules(Arrays.asList(getModules()));
+				throw oce;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	protected void performDeployment(CloudFoundryApplicationModule appModule, IProgressMonitor monitor)
+			throws CoreException {
+		if (!appModule.isDeployed()) {
+			super.performDeployment(appModule, monitor);
+		}
+	}
+
+}
